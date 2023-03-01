a51f9cd074de9a2e6d626f7045c6da275987e8d9
crossingbyX0 <tungpahe150361@fpt.edu.vn>
2023-03-02 01:32:33 +0700

    create club

--- Source/FLMS-FrontEnd/src/app/flms/flms-routing.module.ts
+++ Source/FLMS-FrontEnd/src/app/flms/flms-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateClubComponent } from './club/create-club/create-club.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'create-club',
+    component: CreateClubComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- Source/FLMS-FrontEnd/src/app/flms/flms.module.ts
+++ Source/FLMS-FrontEnd/src/app/flms/flms.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FlmsRoutingModule } from './flms-routing.module';
+import { CreateClubComponent } from './club/create-club/create-club.component';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CreateClubComponent],
   imports: [
     CommonModule,
-    FlmsRoutingModule
+    FlmsRoutingModule, 
+    MatInputModule
   ]
 })
 export class FlmsModule { }
