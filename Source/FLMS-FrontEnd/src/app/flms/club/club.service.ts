fbff33ac59001d0a56444e4fd3171b9c5b1297cf
crossingbyX0 <tungpahe150361@fpt.edu.vn>
2023-03-03 01:28:02 +0700

    create club done

--- Source/FLMS-FrontEnd/src/app/flms/club/club.service.ts
+++ Source/FLMS-FrontEnd/src/app/flms/club/club.service.ts
@@ -6,26 +6,28 @@ import { AddClubResponse } from '../../models/add-club-response.model';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { ClubDetail } from '../../models/club-detail.model';
 import { AddClub } from '../../models/club-detail.model';
+import { token } from '../../models/token.model';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClubService {
-
-  public club: Observable<AddClub>;
-  public userid: number;
-
+  private headers: HttpHeaders;
+  token: token;
 
   constructor(
     private router: Router,
     private http: HttpClient
 
   ) {
-
+    this.token = JSON.parse(localStorage.getItem('user'));
+    this.headers = new HttpHeaders().set('Authorization', `Bearer ${this.token.accessToken}`);
   }
 
   addClub(club: AddClub) {
-    return this.http.post(`${environment.apiUrl}/api/Club/CreateClub`, club)
+    return this.http.post(`${environment.apiUrl}/api/Club/CreateClub`, club,{headers: this.headers} )
   }
 
 
