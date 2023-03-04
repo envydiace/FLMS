a42550a90189c07373189bc4e57cd59b3269c775
Meowinosis <ntphon37@gmail.com>
2023-03-04 12:23:46 +0700

    Send mail ver0.2
    Add BaseMailResponse

---
+++ Source/FLMS_BackEnd/FLMS_BackEnd/Response/BaseMailResponse.cs
@@ -0,0 +1,15 @@
+﻿using DocumentFormat.OpenXml.Presentation;
+using FLMS_BackEnd.Utils;
+using System.Text.Json.Serialization;
+
+namespace FLMS_BackEnd.Response
+{
+    public class BaseMailResponse : BaseResponse
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+
+        public string? MessageMailCode { get; set; } = null;
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? MailMessage { get { return MessageCode != null ? Constants.SystemMessage.GetValueOrDefault(MessageCode) : null; } }
+    }
+}
