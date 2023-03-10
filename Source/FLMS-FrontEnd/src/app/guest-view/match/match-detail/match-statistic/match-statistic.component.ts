c14140ba5f6e1e6abcc27beeb99d6e5f23aaa004
ChauMTV <unknowquest@yahoo.com>
2023-03-10 20:39:32 +0700

    diplay match stats data

--- Source/FLMS-FrontEnd/src/app/guest-view/match/match-detail/match-statistic/match-statistic.component.ts
+++ Source/FLMS-FrontEnd/src/app/guest-view/match/match-detail/match-statistic/match-statistic.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { map, tap } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
 import { MatchService } from './../../match.service';
-import { MatchStats } from '../../../../models/match-statistics-model';
+import { ClubStats, MatchStats } from '../../../../models/match-statistics-model';
 import { MatchStatisticsResponse } from '../../../../models/match-statistic-response-model';
 @Component({
   selector: 'app-match-statistic',
@@ -12,6 +12,12 @@ import { MatchStatisticsResponse } from '../../../../models/match-statistic-resp
 export class MatchStatisticComponent implements OnInit {
   matchStats: MatchStats;
   matchId: number;
+  home: ClubStats;
+  away: ClubStats;
+
+  displayedColumns: string[] = ['home', 'type', 'away'];
+  dataSource = ELEMENT_DATA;
+
   constructor(
     private MatchService: MatchService,
     private route: ActivatedRoute,
@@ -26,10 +32,37 @@ export class MatchStatisticComponent implements OnInit {
   }
 
   initDataSource() {
-    this.MatchService.getMatchStatsById(this.matchId).pipe(map((res: MatchStatisticsResponse) => this.matchStats = res)).subscribe();
+    this.MatchService.getMatchStatsById(this.matchId).pipe(map((res: MatchStatisticsResponse) => {
+      this.home = res.home,
+      this.away = res.away
+    })).subscribe(() => {
+      this.bindDataIntoHome(this.home);
+    });
   }
 
   getMatchStatsById(matchId: number) {
     this.MatchService.getMatchStatsById(this.matchId).pipe(map((res: MatchStatisticsResponse) => this.matchStats = res)).subscribe();
   }
+
+  bindDataIntoHome (home: ClubStats) {
+    ELEMENT_DATA[0].home = home.shot;
+  }
+
 }
+
+export interface PeriodicElement {
+  home: number;
+  type: string;
+  away: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {home: 0, type: 'Hydrogen', away: 0},
+  {home: 0, type: 'Helium', away: 0},
+  {home: 0, type: 'Lithium', away: 0},
+  {home: 0, type: 'Beryllium', away: 0},
+  {home: 0, type: 'Boron', away: 0},
+  {home: 0, type: 'Carbon', away: 0},
+  {home: 0, type: 'Nitrogen', away: 0},
+  {home: 0, type: 'Oxygen', away: 0}
+];
