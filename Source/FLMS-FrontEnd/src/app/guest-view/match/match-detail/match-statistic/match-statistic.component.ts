99fd51510f85adb67534c7c40def432f87bba70d
ChauMTV <unknowquest@yahoo.com>
2023-03-10 12:50:28 +0700

    Add Call API

--- Source/FLMS-FrontEnd/src/app/guest-view/match/match-detail/match-statistic/match-statistic.component.ts
+++ Source/FLMS-FrontEnd/src/app/guest-view/match/match-detail/match-statistic/match-statistic.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { map, tap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { MatchService } from './../../match.service';
+import { MatchStats } from '../../../../models/match-statistics-model';
+import { MatchStatisticsResponse } from '../../../../models/match-statistic-response-model';
 @Component({
   selector: 'app-match-statistic',
   templateUrl: './match-statistic.component.html',
   styleUrls: ['./match-statistic.component.scss']
 })
 export class MatchStatisticComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  matchStats: MatchStats;
+  matchId: number;
+  constructor(
+    private MatchService: MatchService,
+    private route: ActivatedRoute,
+  ) {
+    this.route.queryParams.subscribe(params => {
+      this.matchId = params['matchId'];
+    });
   }
 
+  ngOnInit(): void {
+    this.initDataSource();
+  }
+
+  initDataSource() {
+    this.MatchService.getMatchStatsById(this.matchId).pipe(map((res: MatchStatisticsResponse) => this.matchStats = res)).subscribe();
+  }
+
+  getMatchStatsById(matchId: number) {
+    this.MatchService.getMatchStatsById(this.matchId).pipe(map((res: MatchStatisticsResponse) => this.matchStats = res)).subscribe();
+  }
 }
