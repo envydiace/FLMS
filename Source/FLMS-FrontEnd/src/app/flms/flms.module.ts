9aec91ce4978be40e0d5d4f4e873ace0347b5b8d
ChauMTV <unknowquest@yahoo.com>
2023-03-13 05:24:18 +0700

    Add Change Password API + Club Manager  layouts

--- Source/FLMS-FrontEnd/src/app/flms/flms.module.ts
+++ Source/FLMS-FrontEnd/src/app/flms/flms.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { FlmsRoutingModule } from './flms-routing.module';
 import { FlmsComponent } from './flms.component';
 import { MatInputModule } from '@angular/material/input';
@@ -21,13 +20,32 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from '@angular/material/snack-bar';	
+import { MatChipsModule } from '@angular/material/chips';	
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';	
+import { MatNativeDateModule } from '@angular/material/core';	
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatListModule } from '@angular/material/list';
+import { CdkTableModule } from '@angular/cdk/table';
+import { ClubDetailComponent } from './club/club-detail/club-detail.component';
+import { ClubListComponent } from './club/club-list/club-list.component';
+import { ClubScheduleComponent } from './club/club-detail/club-schedule/club-schedule.component';
+import { ClubPlayerComponent } from './club/club-detail/club-player/club-player.component';
+import { PopUpAddPlayerComponent } from './club/club-detail/pop-up-add-player/pop-up-add-player.component';
 
 @NgModule({
   declarations: [
     FlmsComponent,
     CreateClubComponent,
     ViewProfileComponent,
-    RequestListComponent
+    RequestListComponent,
+    ClubDetailComponent,
+    ClubListComponent,
+    ClubScheduleComponent,
+    ClubPlayerComponent,
+    PopUpAddPlayerComponent
   ],
   imports: [
     CommonModule,
@@ -48,7 +66,16 @@ import { MatSortModule } from '@angular/material/sort';
     MatCardModule,
     FormsModule,
     FlmsRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTabsModule,
+    MatListModule,
+    CdkTableModule,
+    MatSelectModule
   ]
 })
 export class FlmsModule { }
