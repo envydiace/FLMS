7a65cf6a65275675e7d83c54f26b3466baa38400
envydiace <ninhducanh216@gmail.com>
2023-03-14 23:11:44 +0700

    fee id

--- Source/FLMS_BackEnd/FLMS_BackEnd/DTO/LeagueFeeDTO.cs
+++ Source/FLMS_BackEnd/FLMS_BackEnd/DTO/LeagueFeeDTO.cs
@@ -2,6 +2,7 @@
 {
     public class LeagueFeeDTO
     {
+        public int LeagueFeeId { get; set; }
         public int LeagueId { get; set; }
         public string ExpenseKey { get; set; } = null!;
         public string ExpenseName { get; set; } = null!;
