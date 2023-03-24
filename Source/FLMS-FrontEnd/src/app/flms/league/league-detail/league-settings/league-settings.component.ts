8c9050331d496121cf30eb95c340ce99d3728670
nguyenminhsonn <sown2432001@gmail.com>
2023-03-25 02:44:16 +0700

    update league settings

--- Source/FLMS-FrontEnd/src/app/flms/league/league-detail/league-settings/league-settings.component.ts
+++ Source/FLMS-FrontEnd/src/app/flms/league/league-detail/league-settings/league-settings.component.ts
@@ -1,4 +1,10 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { ClubListByLeagueResponse } from 'src/app/models/club-list-by-league-response.model';
+import { ClubListByLeague } from 'src/app/models/club-list-by-league.model';
+import { LeagueService } from '../../league.service';
 
 @Component({
   selector: 'app-league-settings',
@@ -6,10 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./league-settings.component.scss']
 })
 export class LeagueSettingsComponent implements OnInit {
+  listClubByLeague: ClubListByLeague[] = [];
+  leagueId: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private leagueService: LeagueService,
+  ) {
+    this.route.queryParams.subscribe(params => {
+      this.leagueId = params['leagueId'];
+    });
   }
 
+  ngOnInit(): void {
+    this.initDataSource();
+  }
+
+  initDataSource() {
+    this.findClubByLeague();
+  }
+
+  findClubByLeague() {
+    this.leagueService.findClubByLeague(this.leagueId + '', '').pipe(
+      map((listClubByLeague: ClubListByLeagueResponse) => this.listClubByLeague = listClubByLeague.clubs)
+    ).subscribe();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+
+    } else {
+      if (event.previousContainer.data[event.currentIndex] != undefined) {
+        let oldtarget = event.previousContainer.data[event.previousIndex];
+        event.previousContainer.data[event.previousIndex] = event.container.data[event.currentIndex];
+        event.container.data[event.currentIndex] = oldtarget;
+      } else {
+        transferArrayItem(event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex);
+      }
+    }
+  }
 }
