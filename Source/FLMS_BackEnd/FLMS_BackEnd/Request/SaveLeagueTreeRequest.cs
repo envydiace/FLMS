a6d10626a018477bc3a419abee8472e6e6d6cc63
envydiace <ninhducanh216@gmail.com>
2023-03-25 01:28:54 +0700

    add participation

---
+++ Source/FLMS_BackEnd/FLMS_BackEnd/Request/SaveLeagueTreeRequest.cs
@@ -0,0 +1,10 @@
+﻿using FLMS_BackEnd.DTO;
+
+namespace FLMS_BackEnd.Request
+{
+    public class SaveLeagueTreeRequest
+    {
+        public int LeagueId { get; set; }
+        public List<TreeNodeSaveDTO> ListNode { get; set; } = new List<TreeNodeSaveDTO>();
+    }
+}
