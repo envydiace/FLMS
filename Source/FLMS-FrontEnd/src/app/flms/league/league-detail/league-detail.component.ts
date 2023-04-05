7f84f75985f2b4d1709edb46ab8ccc21a107e5b6
NotPATtrick <anhtungphan1108@gmail.com>
2023-04-06 05:53:52 +0700

    fix size league schedule table

--- Source/FLMS-FrontEnd/src/app/flms/league/league-detail/league-detail.component.ts
+++ Source/FLMS-FrontEnd/src/app/flms/league/league-detail/league-detail.component.ts
@@ -19,6 +19,7 @@ export class LeagueDetailComponent implements OnInit {
   leagueId: number;
   private headers: HttpHeaders;
   token: token;
+  // defaultLogo: string = './../../../../assets/image/clubDefaultLogo.png';
   defaultLogo: string = './../../../../assets/image/default-logo.png';
 
 
