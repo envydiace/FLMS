080ae6758854c5c39dade9ae0680eb86238337e5
NotPATtrick <anhtungphan1108@gmail.com>
2023-04-09 21:52:36 +0700

    create club added fanpage create club with no logo done create club change button to create create club redirect to my list club edit player name no more validate edit league front end fanpage resize

--- Source/FLMS-FrontEnd/src/app/models/club-detail.model.ts
+++ Source/FLMS-FrontEnd/src/app/models/club-detail.model.ts
@@ -44,7 +44,7 @@ export interface AddClub {
     clubName: string,
     email: string,
     phoneNumber: number,
-    socialCont: number,
+    fanPage: number,
     logo: string,
     kit: string
 }
@@ -54,14 +54,14 @@ export class AddClub implements AddClub{
         clubName: string,
         email: string,
         phoneNumber: number,
-        socialCont: number,
+        fanPage: number,
         logo: string,
         kit: string
     ){
         this.clubName = clubName;
         this.email = email;
         this.phoneNumber = phoneNumber;
-        this.socialCont = socialCont;
+        this.fanPage = fanPage;
         this.logo = logo;
         this.kit = kit;
     }
