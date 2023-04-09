fa9b91357c43969a78aec2d1bba492e73c21633e
nguyenminhsonn <sown2432001@gmail.com>
2023-04-09 10:41:51 +0700

    update filter league list

--- Source/FLMS-FrontEnd/src/app/guest-view/club/club-list/club-list.component.ts
+++ Source/FLMS-FrontEnd/src/app/guest-view/club/club-list/club-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ClubService } from './../club.service';
 import { ClubList } from 'src/app/models/club-list.model';
-import { map, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, map, tap } from 'rxjs/operators';
 import { PageEvent } from '@angular/material/paginator';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-club-list',
@@ -12,16 +13,17 @@ import { AuthService } from 'src/app/auth/auth.service';
   styleUrls: ['./club-list.component.scss']
 })
 export class ClubListComponent implements OnInit {
-  clubName: string = null;
-  managerName: string = null;
+  // clubName: string = null;
+  // managerName: string = null;
   clubList: ClubList = null;
   pageEvent: PageEvent;
   defaultLogo: string = './../../../../assets/image/clubDefaultLogo.png';
   userRole: string;
-
+  filterForm: FormGroup;
 
   constructor(
     private clubService: ClubService,
+    private formBuilder: FormBuilder,
     private router: Router,
     private authService: AuthService,
 
@@ -29,8 +31,55 @@ export class ClubListComponent implements OnInit {
 
   ngOnInit(): void {
     this.initDataSource();
+    this.createForm();
+
     this.userRole = this.authService.getUserRole();
 
+    this.onChangeClubNameFilter();
+    this.onChangeManagerNameFilter();
+  }
+
+  getControl(name: string) {
+    return this.filterForm.get(name) as FormControl;
+  }
+
+  createForm() {
+    this.filterForm = this.formBuilder.group({
+      'clubName': [null],
+      'managerName': [null],
+    });
+  }
+
+  onChangeClubNameFilter() {
+    let clubName = this.getControl('clubName').value;
+    let managerName = this.getControl('managerName').value;
+
+    if (clubName == null) clubName = '';
+    if (managerName == null) managerName = '';
+
+    this.getControl('clubName').valueChanges
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((term) => {
+        this.clubService.getListClubFilter(term, managerName, 1, 8).pipe(
+          map((clubList: ClubList) => this.clubList = clubList)
+        ).subscribe()
+      });
+  }
+
+  onChangeManagerNameFilter() {
+    let clubName = this.getControl('clubName').value;
+    let managerName = this.getControl('managerName').value;
+
+    if (clubName == null) clubName = '';
+    if (managerName == null) managerName = '';
+
+    this.getControl('managerName').valueChanges
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((term) => {
+        this.clubService.getListClubFilter(clubName, term, 1, 8).pipe(
+          map((clubList: ClubList) => this.clubList = clubList)
+        ).subscribe()
+      });
   }
 
   initDataSource() {
@@ -42,33 +91,26 @@ export class ClubListComponent implements OnInit {
   onPaginateChange(event: PageEvent) {
     let page = event.pageIndex;
     let size = event.pageSize;
-
-    if (this.clubName == null) {
+    let clubName = this.getControl('clubName').value;
+    let managerName = this.getControl('managerName').value;
+    if (clubName == null && managerName == null) {
       page = page + 1;
       this.clubService.findAll(page, size).pipe(
         map((clubList: ClubList) => this.clubList = clubList)
       ).subscribe();
     } else {
       page = page + 1;
-      if (this.clubName == null) this.clubName = '';
-      if (this.managerName == null) this.managerName = '';
-      this.clubService.getListClubFilter(this.clubName, this.managerName, page, size).pipe(
+      if (clubName == null || clubName == undefined) clubName.value = '';
+      if (managerName == null || managerName == undefined) managerName = '';
+      this.clubService.getListClubFilter(clubName, managerName, page, size).pipe(
         map((clubList: ClubList) => this.clubList = clubList)
       ).subscribe()
     }
 
   }
-
-  findClubsByName(clubName: string, managerName: string) {
-    if (clubName == null) clubName = '';
-    if (managerName == null) managerName = '';
-    this.clubService.getListClubFilter(clubName, managerName, 1, 8).pipe(
-      map((clubList: ClubList) => this.clubList = clubList)
-    ).subscribe()
-  }
-
+  
   navigateToClubDetail(id: number) {
-    this.router.navigate(['/club-info'], { queryParams: {clubId : id}});
+    this.router.navigate(['/club-info'], { queryParams: { clubId: id } });
   }
 
 
