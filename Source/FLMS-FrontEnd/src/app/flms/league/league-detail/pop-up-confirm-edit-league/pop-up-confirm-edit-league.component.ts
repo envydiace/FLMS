6e5e50e7069582e184491257bfef42065cd59253
NotPATtrick <anhtungphan1108@gmail.com>
2023-04-19 22:16:38 +0700

    edit league and confirm edit

--- Source/FLMS-FrontEnd/src/app/flms/league/league-detail/pop-up-confirm-edit-league/pop-up-confirm-edit-league.component.ts
+++ Source/FLMS-FrontEnd/src/app/flms/league/league-detail/pop-up-confirm-edit-league/pop-up-confirm-edit-league.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { CommonService } from 'src/app/common/common/common.service';
+import { LeagueService } from '../../league.service';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pop-up-confirm-edit-league',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pop-up-confirm-edit-league.component.scss']
 })
 export class PopUpConfirmEditLeagueComponent implements OnInit {
+  loading = false;
+  
+  constructor(
+    private commonService: CommonService,
+    private leagueService: LeagueService,
+    public dialogRe: MatDialog,
+    public dialogRef: MatDialogRef<PopUpConfirmEditLeagueComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      leagueId: number;
+      conStatus: boolean;
+    },
+    private router: Router ,
+  ) { }
 
-  constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit() {
+    // stop here if form is invalid
+    this.loading = true;
+    this.data.conStatus = this.loading;
+    this.dialogRef.close();
+
+  }
 }
